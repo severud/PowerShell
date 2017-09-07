f71edfc09b26ddc105486f1a30bbb173fb4e4db6
Kevin Severud <severud@gmail.com>
2017-09-07 14:17:49 -0700

    Update RuckZuck-update-apps.ps1

--- RuckZuck-update-apps.ps1
+++ RuckZuck-update-apps.ps1
@@ -3,12 +3,24 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     Break
 }
 
+#Check for RuckZuck provider for PowerShell installation status
+if (!(Test-Path "C:\Program Files\RuckZuck for OneGet\RuckZuckProvider.dll")) {
+    Write-Host "Installing RuckZuck provider for PowerShell Packagemanagement…" -ForegroundColor Magenta
+    $uri = "https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=ruckzuck&DownloadId=1457985&FileTime=131329510729830000&Build=21053"
+    $file = "$env:temp/RuckZuck provider for OneGet_x64.msi"
+    Invoke-WebRequest -Uri $uri -Method get -OutFile $file
+    Unblock-File $file
+    Start-Process -FilePath $file -ArgumentList "/qn" -Wait 
+    rm $file -Force
+} else {
+    Write-Host "RuckZuck provider for PowerShell Packagemanagement previously installed ;-)" -ForegroundColor Green
+}
+
 $PackageUpdates = Find-Package -ProviderName RuckZuck -Updates -WarningAction SilentlyContinue
 if (!$PackageUpdates) { Write-Host "RuckZuck: No updates found." -ForegroundColor Green}
 else {
     foreach ($Package in $PackageUpdates.PackageFilename) {
-        #Write-Host ("Current package: ") -NoNewline
-        Write-Output (Find-Package -ProviderName RuckZuck $Package).Name
+        Write-Information -messagedata (Find-Package -ProviderName RuckZuck $Package).Name -InformationAction Continue
         Install-Package -ProviderName RuckZuck -Name $Package
     }
 }
