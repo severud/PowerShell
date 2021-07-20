30586d327289226657c2c77468481ca3f846ed22
Kevin Severud <severud@gmail.com>
2021-07-20 11:21:40 -0700

    Create Get-RedirectedUrl.ps1

---
+++ AppDeploy/Get-RedirectedUrl.ps1
@@ -0,0 +1,21 @@
+Function Get-RedirectedUrl {
+    Param (
+    [Parameter(Mandatory=$true)]
+    [String]$url
+    )
+    
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$true
+    
+    try{
+    $response=$request.GetResponse()
+    $response.ResponseUri.AbsoluteUri
+    $response.Close()
+    }
+    
+    catch{
+    "ERROR: $_"
+    }
+    
+    }
+    
